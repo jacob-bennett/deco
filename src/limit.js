43bebdac93fba347d3c8593fe9d40171929c8bc6
Jacob Bennett <jacob@jacobbennett.tech>
2025-10-27 15:40:27 -0300

    tsdocs for generating ts types

--- src/limit.js
+++ src/limit.js
@@ -1,14 +1,32 @@
+/**
+ * Wraps an asynchronous function to limit the number of concurrent executions.
+ *
+ * @template {any[]} Args - Argument types of the original function
+ * @template Return - Return type of the original function
+ * @param {(...args: Args) => Promise<Return> | Return} fn - The function to wrap
+ * @param {number} limit - Maximum number of concurrent executions allowed
+ *
+ * @returns {(...args: Args) => Promise<Return>} Wrapped function that respects the concurrency limit
+ */
 export const limit = (fn, limit) => {
     validateArgs(fn, limit);
 
+    /**
+     * @type {Array<{ args: Args; resolve: (value: Return) => void }>}
+     */
     const queue = [];
     let processing = 0;
 
     const next = () => {
+        // @ts-ignore This will never be undefined.
         const {args, resolve} = queue.shift();
         run(args).then((result) => resolve(result))
     }
 
+    /**
+     * @param {Args} args
+     * @returns {Promise<Return>}
+     */
     const run = async args => {
         processing++
         const result = await Promise.resolve()
@@ -31,6 +49,12 @@ export const limit = (fn, limit) => {
     }
 }
 
+/**
+ * @param {Function} fn
+ * @param {number} limit
+ *
+ * @throws {TypeError}
+ */
 const validateArgs = (fn, limit) => {
     if (typeof fn !== 'function') {
         throw new TypeError('parameter must be a function');
