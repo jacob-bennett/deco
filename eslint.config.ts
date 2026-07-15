732a2d6b90c92ba08fb72d1f12e2e697851c798e
Jacob Bennett <jacob@jacobbennett.tech>
2026-07-15 10:07:17 -0300

    add missing install step

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,10 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
+import tsEslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 export default defineConfig(
   eslint.configs.recommended,
-  tseslint.configs.strict,
+  tsEslint.configs.strict,
   prettier,
 );
